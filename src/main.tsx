cb7e3564ade701dc80428a87dcc7ac152c43bda7
Alejandro Coca <alxmcr@outlook.com>
2024-03-04 00:25:26 -0400

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CardRouterFallback from './components/react-router/cards/CardRouterFallback';
 import './index.scss';
 import { routes } from './router/routes';
 
@@ -9,7 +10,10 @@ const appRouter = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={appRouter} />
+    <RouterProvider
+      router={appRouter}
+      fallbackElement={<CardRouterFallback />}
+    />
     <Toaster />
   </React.StrictMode>,
 );
