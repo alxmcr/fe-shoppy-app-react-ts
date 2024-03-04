44d0bd4e100197dc8b1327a74a50efb05dacf2c7
Alejandro Coca <alxmcr@outlook.com>
2024-03-04 01:34:57 -0400

    wip

--- src/components/product-list-page/sections/SectionProductList/index.tsx
+++ src/components/product-list-page/sections/SectionProductList/index.tsx
@@ -1,10 +1,52 @@
+import React from 'react';
+import {
+  ProductFilterConditions,
+  ProductFilterConditionsContext,
+} from '../../../../providers/ProductFilterConditionsProvider/ProductFilterConditionsContext';
+import { ProductFilterModalContext } from '../../../../providers/ProductFilterModalProvider/ProductFilterModalContext';
 import BoxProductList from '../../boxes/BoxProductList';
 import './SectionProductList.scss';
 
 export default function SectionProductList() {
+  const { showFilterProductModal, setShowFilterProductModal } =
+    React.useContext(ProductFilterModalContext);
+  const {
+    setFilterProductName,
+    setFilterProductFreqTime,
+    setFilterProductTags,
+  } = React.useContext(ProductFilterConditionsContext);
+
+  const applyProductFilter = (filterConditions: ProductFilterConditions) => {
+    setFilterProductName(filterConditions.filterProductName);
+    setFilterProductFreqTime(filterConditions.filterProductFreqTime);
+    // modal
+    setShowFilterProductModal(false);
+  };
+
+  const resetProductFilter = () => {
+    setFilterProductName('');
+    setFilterProductFreqTime(null);
+    // Filter tags
+    setFilterProductTags([]);
+  };
+
+  const onClickCloseModal = () => {
+    setShowFilterProductModal(false);
+  };
+
   return (
     <section className="section-product-list">
+      <BoxProductFilterTags />
       <BoxProductList />
+      {showFilterProductModal ? (
+        <ModalContainer>
+          <ModalProductFilter
+            onCloseModal={onClickCloseModal}
+            applyProductFilter={applyProductFilter}
+            resetProductFilter={resetProductFilter}
+          />
+        </ModalContainer>
+      ) : null}
     </section>
   );
 }
