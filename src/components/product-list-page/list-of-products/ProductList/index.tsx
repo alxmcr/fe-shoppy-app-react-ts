59df911b5b916f34b176cb36d699b9aab4fece58
Alejandro Coca <alxmcr@outlook.com>
2024-03-04 01:56:44 -0400

    wip

--- src/components/product-list-page/list-of-products/ProductList/index.tsx
+++ src/components/product-list-page/list-of-products/ProductList/index.tsx
@@ -1,15 +1,26 @@
-import { Product } from '../../../../@types/appTypes';
+import React from 'react';
 import CardCongratsShoppingListEmpty from '../../cards/CardCongratsShoppingListEmpty';
 import CardFilterProductsEmpty from '../../cards/CardFilterProductsEmpty';
 import ProductListItem from '../ProductListItem';
 import './ProductList.scss';
+import { Product } from '../../../../@types/appTypes';
+import { ProductFilterConditionsContext } from '../../../../providers/ProductFilterConditionsProvider/ProductFilterConditionsContext';
+import { onFilterProductsByConditions } from '../../../../helpers/helpers-filter';
 
 type ProductListProps = {
   products: Product[];
 };
 
 export default function ProductList({ products = [] }: ProductListProps) {
-  const productsFiltered = [...products];
+  const { filterProductName, filterProductFreqTime } = React.useContext(
+    ProductFilterConditionsContext,
+  );
+  const productsFiltered = products.filter((product) =>
+    onFilterProductsByConditions(product, {
+      filterProductName,
+      filterProductFreqTime,
+    }),
+  );
 
   if (products.length === 0) {
     return <CardCongratsShoppingListEmpty />;
