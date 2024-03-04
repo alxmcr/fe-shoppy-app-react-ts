33c16dc1ec68b39aeff5665a2f9b8a25fd7bf54c
Alejandro Coca <alxmcr@outlook.com>
2024-03-03 23:36:50 -0400

    wip

---
+++ src/constants/constants-app.ts
@@ -0,0 +1,13 @@
+export const KEY_TOKEN = 'token';
+
+export const LoadingStates = {
+  IDLE: 'IDLE',
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+};
+
+export const FILTER_KEYS = {
+  NAME: 'name',
+  FREQ_TIME: 'freq-time',
+};
