48fe10980fc7adfb13f99bf9602d6327cd6009e8
Alejandro Coca <alxmcr@outlook.com>
2024-03-04 14:25:50 -0400

    jest testing some configurations

--- src/components/page-home/cards/CardIntro/CardIntro.test.tsx
+++ src/components/page-home/cards/CardIntro/CardIntro.test.tsx
@@ -1,8 +1,22 @@
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import CardIntro from '.';
 
 describe('CardIntro test', () => {
   test('renders correctly', () => {
+    const APP_NAME = 'Shopy';
+    const APP_DESCRIPTION = `Let's plan your purchases with our smart shopping list`;
+
     render(<CardIntro />);
+
+    // UI elements
+    const title = screen.getByRole('heading', { name: APP_NAME });
+    const description = screen.getByRole('paragraph', {
+      name: APP_DESCRIPTION,
+    });
+
+    // assertions
+    expect(title).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
   });
 });
