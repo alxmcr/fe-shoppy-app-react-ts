94ed090edf4a226fefa03fca1479efb9a77a9bd5
Alejandro Coca <alxmcr@outlook.com>
2024-03-04 13:45:55 -0400

    basic test

---
+++ src/components/page-home/cards/CardIntro/CardIntro.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import CardIntro from '.';
+
+describe('CardIntro test', () => {
+  test('renders correctly', () => {
+    render(<CardIntro />);
+  });
+});
