0dd974cffac6992869142e1435957b4037e22f29
Alejandro Coca <alxmcr@outlook.com>
2024-03-05 14:05:10 -0400

    ESLint: testing-library and jest-dom setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:jest-dom/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   overrides: [
@@ -33,11 +34,27 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react-refresh', '@typescript-eslint', 'react'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'react',
+    'testing-library',
+    'jest-dom',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // testing-library
+    'testing-library/await-async-queries': 'error',
+    'testing-library/no-await-sync-queries': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
+    // jest-dom
+    'jest-dom/prefer-checked': 'error',
+    'jest-dom/prefer-enabled-disabled': 'error',
+    'jest-dom/prefer-required': 'error',
+    'jest-dom/prefer-to-have-attribute': 'error',
   },
 };
