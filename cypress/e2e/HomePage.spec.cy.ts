fbfd5021d05611f025a17c6979a1b07941fc6d61
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-03-05 01:38:12 -0400

    cypress - e2e - home page (#26)

--- cypress/e2e/HomePage.spec.cy.ts
+++ cypress/e2e/HomePage.spec.cy.ts
@@ -9,5 +9,37 @@ describe('HomePage e2e spec', () => {
     // Assertions
     cy.get('[data-cy="cy-token-input"]').should('be.visible');
     cy.get('[data-cy="cy-token-input"]').type(TOKEN_TEST);
+    cy.get('[data-cy="button-join-shopping-list"]').should('be.visible');
+    cy.get('[data-cy="button-join-shopping-list"]').click();
+
+    // we should be redirected to /list
+    cy.url().should('include', '/list');
+
+    cy.get('[data-cy="cy-header-token"]').should('be.visible');
+    cy.get('[data-cy="cy-header-token"]').should('have.text', TOKEN_TEST);
+  });
+
+  it('create a new shopping list', () => {
+    // Assertions
+    cy.get('[data-cy="button-create-new-list"]').should('be.visible');
+    cy.get('[data-cy="button-create-new-list"]').click();
+
+    // we should be redirected to /list
+    cy.url().should('include', '/list');
+
+    cy.get('[data-cy="cy-empty-list-title"]').should('be.visible');
+    cy.get('[data-cy="cy-empty-list-title"]').should('have.text', 'Congrats!');
+
+    cy.get('[data-cy="cy-empty-list-message"]').should('be.visible');
+    cy.get('[data-cy="cy-empty-list-message"]').should(
+      'have.text',
+      'Your shopping list is empty!',
+    );
+
+    cy.get('[data-cy="cy-empty-list-link-add-product"]').should('be.visible');
+    cy.get('[data-cy="cy-empty-list-link-add-product"]').should(
+      'have.text',
+      'Add new product',
+    );
   });
 });
